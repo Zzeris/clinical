e57d9e6b802ba8eaca8eac5eb3f6824726875ac4
zzeris <zzeris@gmail.com>
2019-10-22 19:47:54 -0300

    Add daily rule

--- src/controllers/dailyRuleController.js
+++ src/controllers/dailyRuleController.js
@@ -1,8 +1,31 @@
-module.exports = {
-    store(req, res) {
+const fs = require('fs');
+const dataFilePath = './data.json';
 
+module.exports = {
+    async store(req, res) {
+        const fileBuffer = fs.readFileSync(dataFilePath, 'utf-8');
+
+        const dataJson = JSON.parse(fileBuffer);
+
+        dataJson.diariamente = req.body;
+
+        const dataString = JSON.stringify(dataJson);
+
+        fs.writeFileSync(dataFilePath, dataString);
+
+        res.send('Horário diário cadastrado com sucesso.');
     },
-    delete(req, res) {
-        
+    delete(_, res) {
+        const fileBuffer = fs.readFileSync(dataFilePath, 'utf-8');
+
+        const dataJson = JSON.parse(fileBuffer);
+
+        dataJson.diariamente = {};
+
+        const dataString = JSON.stringify(dataJson);
+
+        fs.writeFileSync(dataFilePath, dataString);
+
+        res.send('Horário diário excluido com sucesso.');
     }
 }
